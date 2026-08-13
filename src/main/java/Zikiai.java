3a14d54b90b0159ec7336840a3f6475acab53957
Ai ZiQi <zikiai0612@gmail.com>
2026-08-14 01:58:45 +0800

    Update Zikiai.java: Add mark command to track completed tasks

--- src/main/java/Zikiai.java
+++ src/main/java/Zikiai.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Runs the Zikiai chatbot and responds to task commands entered by the user.
+ */
 public class Zikiai {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        int counter = 0;
+        List<Task> tasks = new ArrayList<>();
 
         String banner = " _____ _ _    _       _\n"
                 + "|__  /(_) | _(_) __ _(_)\n"
@@ -17,21 +21,64 @@ public class Zikiai {
         System.out.println("Hello! I'm Zikiai.");
         System.out.println("What can I do for you?");
         System.out.println(line);
+
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
             }
+
+            if (input.matches("mark \\d+")) {
+                String numberText = input.substring(5);
+                int taskNumber = Integer.parseInt(numberText);
+                int taskIndex = taskNumber - 1;
+                if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                    System.out.println(line);
+                    System.out.println("That task number does not exist.");
+                    System.out.println(line);
+                    continue;
+                }
+
+                Task task = tasks.get(taskIndex);
+                task.markAsDone();
+
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("    " + task.getDescription());
+                System.out.println(line);
+                continue;
+            }
+
+            if (input.matches("unmark \\d+")) {
+                String numberText = input.substring(7);
+                int taskNumber = Integer.parseInt(numberText);
+                int taskIndex = taskNumber - 1;
+                if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                    System.out.println(line);
+                    System.out.println("That task number does not exist.");
+                    System.out.println(line);
+                    continue;
+                }
+
+                Task task = tasks.get(taskIndex);
+                task.markAsNotDone();
+
+                System.out.println(line);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("    " + task.getDescription());
+                System.out.println(line);
+                continue;
+            }
             if (input.equals("list")) {
                 System.out.println(line);
-                for (int i = 0; i < counter; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + "." + tasks.get(i).getDescription());
                 }
                 System.out.println(line);
                 continue;
             }
-            tasks[counter] = input;
-            counter++;
+            tasks.add(new Task(input));
             System.out.println(line);
             System.out.println("    added: " + input);
             System.out.println(line);
