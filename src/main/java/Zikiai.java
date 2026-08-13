164c4e12c1d5249d2d819347d1d6cc3919988b9b
Ai ZiQi <zikiai0612@gmail.com>
2026-08-13 17:16:24 +0800

    Update Zikiai.java:
    Add simple greet user and exit

--- src/main/java/Zikiai.java
+++ src/main/java/Zikiai.java
@@ -4,7 +4,14 @@ public class Zikiai {
                 + "|__  /(_) | _(_) __ _(_)\n"
                 + "  / / | | |/ / |/ _` | |\n"
                 + " / /_ | |   <| | (_| | |\n"
-                + "/____||_|_|\\_\\_|\\__,_|_|\n";
+                + "/____||_|_|\\_\\_|\\__,_|_|";
+        String line = "_".repeat(60);
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Zikiai.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
