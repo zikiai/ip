fe3340cbd85cc48bdfef4761819e682bc75c8bb4
Ai ZiQi <zikiai0612@gmail.com>
2026-08-13 16:59:21 +0800

    Update README.md and Zikiai.java:
    Rename DUKE to Zikiai name

---
+++ src/main/java/Zikiai.java
@@ -0,0 +1,10 @@
+public class Zikiai {
+    public static void main(String[] args) {
+        String banner = " _____ _ _    _       _\n"
+                + "|__  /(_) | _(_) __ _(_)\n"
+                + "  / / | | |/ / |/ _` | |\n"
+                + " / /_ | |   <| | (_| | |\n"
+                + "/____||_|_|\\_\\_|\\__,_|_|\n";
+        System.out.println(banner);
+    }
+}
